c1361246a1f13192e806369b8b06d5e1b6bb37ef
MauMatiasG <maomatias1011@gmail.com>
2024-10-28 13:30:26 -0600

    Tarea 9

--- p138-articulo.py
+++ p138-articulo.py
@@ -15,18 +15,24 @@ class Articulo:
 
 # Programa principal
 os.system("cls")
+
 art1 = Articulo('A101', 'Pluma Roja', 888, 0.08)
 print(art1)
+
 art1.cantidad = 999
 art1.precio = 0.99
+
 print("id          = ",art1.id)
 print("descripcion = ",art1.descripcion)
 print("cantidad    = ",art1.cantidad)
 print("precio      = ",art1.precio)
 print("total       = ",art1.obtenerTotal())
+
 art2 = Articulo("A102", "Pluma Azul", 934, 1.2)
 print(art2)
+
 art3 = Articulo("P103", "Lapiz del 12", 456, 0.5)
 print(art3)
+
 total = art1.obtenerTotal() + art2.obtenerTotal() + art3.obtenerTotal()
 print('Total todos:', total)
